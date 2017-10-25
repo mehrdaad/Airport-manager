bc875326d77f6d577179d870b42100ac914a1eb6
rduriancik <r.duriancik@gmail.com>
2017-10-25 23:28:28 +0200

    Created persistence configuration class 'PersistenceConfig' and hibernate configuration in persistence.xml

---
+++ src/main/java/cz/fi/muni/pa165/PersistenceConfig.java
@@ -0,0 +1,39 @@
+package cz.fi.muni.pa165;
+
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class PersistenceConfig {
+
+    @Bean
+    public JpaTransactionManager transactionManager() {
+        return new JpaTransactionManager(entityManagerFactory().getObject());
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(dataSource());
+        factoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+
+        return factoryBean;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.DERBY)
+                .build();
+    }
+
+}
