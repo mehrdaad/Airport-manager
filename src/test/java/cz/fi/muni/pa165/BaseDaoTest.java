45ac57e8dab9e7b0634826985adb3364c0e5c2de
rduriancik <r.duriancik@gmail.com>
2017-10-27 16:12:36 +0200

    Testng replaced with junit. Created BaseDaoTest class with configuration for testing dao implementations. Added ComponentScan annotation in the PersistenceConfig.

---
+++ src/test/java/cz/fi/muni/pa165/BaseDaoTest.java
@@ -0,0 +1,11 @@
+package cz.fi.muni.pa165;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration(classes = PersistenceConfig.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class BaseDaoTest {
+
+}
