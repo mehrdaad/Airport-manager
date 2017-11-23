6eb72ef4e2c122635a3bfc673652385bb1f5349b
xjiranek <475978@mail.muni.cz>
2017-11-23 23:21:42 +0100

    removing wildcard import from steward entity

--- Airport-manager-persistence/src/main/java/cz/fi/muni/pa165/entities/Steward.java
+++ Airport-manager-persistence/src/main/java/cz/fi/muni/pa165/entities/Steward.java
@@ -1,6 +1,10 @@
 package cz.fi.muni.pa165.entities;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
