c01cccbfa3ae7b5319bde92fc1d8a71ec5a1b822
Caky123 <cakl.jan@gmail.com>
2017-11-26 10:20:55 +0100

    AirplaneServiceImpl, AirplaneService updated

--- Airport-manager-service/src/main/java/cz/fi/muni/pa165/service/AirplaneService.java
+++ Airport-manager-service/src/main/java/cz/fi/muni/pa165/service/AirplaneService.java
@@ -1,6 +1,7 @@
 package cz.fi.muni.pa165.service;
 
 import cz.fi.muni.pa165.entities.Airplane;
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -45,6 +46,50 @@ public interface AirplaneService {
     List<Airplane> findAll();
     
     /**
-    *   TODO non trivial fce
-    */
+     * Find all airplanes with given name
+     *
+     * @param name name of airplane
+     *  @return list of all airplanes with given name
+     */
+    List<Airplane> findByName(String name);
+    
+    /**
+     * Find all airplanes with given type
+     *
+     * @param type type of airplane
+     *  @return list of all airplanes with given type
+     */
+    List<Airplane> findByType(String type);
+    
+    /**
+     * Find all airplanes with given capacity
+     *
+     * @param capacity capacity of airplane
+     *  @return list of all airplanes with given capacity and greater capacity
+     */
+    List<Airplane> findByCapacityMin(int capacity);
+    
+    /**
+     * Find all airplanes with given capacity
+     *
+     * @param capacity capacity of airplane
+     *  @return list of all airplanes with given capacity and less capacity
+     */
+    List<Airplane> findByCapacityMax(int capacity);
+    
+    /**
+     * Check if airplane is used after sinceDateTime
+     * 
+     * @param sinceDateTime date after which using is checked
+     * @return list of all used airplanes after sinceDateTime
+     */
+    List<Airplane> findByUsedAfterDateTime(LocalDateTime sinceDateTime);
+    
+    /**
+     * Check if airplane is free after sinceDateTime
+     * 
+     * @param sinceDateTime date after which availibility is checked
+     * @return list of all free airplanes after sinceDateTime
+     */
+    List<Airplane> findByFreeAfterDateTime(LocalDateTime sinceDateTime);
 }
--- Airport-manager-service/src/main/java/cz/fi/muni/pa165/service/AirplaneServiceImpl.java
+++ Airport-manager-service/src/main/java/cz/fi/muni/pa165/service/AirplaneServiceImpl.java
@@ -2,6 +2,9 @@ package cz.fi.muni.pa165.service;
 
 import cz.fi.muni.pa165.dao.AirplaneDao;
 import cz.fi.muni.pa165.entities.Airplane;
+import cz.fi.muni.pa165.entities.Flight;
+import java.time.LocalDateTime;
+import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -13,6 +16,9 @@ import java.util.List;
 public class AirplaneServiceImpl implements AirplaneService {
     @Autowired
     private AirplaneDao airplaneDao;
+    
+    @Autowired
+    private FlightService flightService;
 
     @Override
     public Airplane findById(Long id) {
@@ -39,7 +45,67 @@ public class AirplaneServiceImpl implements AirplaneService {
         return airplaneDao.findAll();
     }
     
-    /**
-     * TODO non trivial fce
-     */
+    @Override
+    public List<Airplane> findByName(String name) {
+        return airplaneDao.findByName(name);
+    }
+    
+    @Override
+    public List<Airplane> findByType(String type) {
+        return airplaneDao.findByType(type);
+    }
+    
+    @Override
+    public List<Airplane> findByCapacityMin(int capacity) {
+        return airplaneDao.findByCapacityMin(capacity);
+    }
+    
+    @Override
+    public List<Airplane> findByCapacityMax(int capacity) {
+        return airplaneDao.findByCapacityMax(capacity);
+    }
+
+    @Override
+    public List<Airplane> findByUsedAfterDateTime(LocalDateTime sinceDateTime) {
+      
+        List<Flight> allFlightSince = flightService.getFlightsSince(sinceDateTime);
+        List<Airplane> allAirplanes = airplaneDao.findAll();
+        
+        if(allFlightSince.isEmpty()){
+            return Collections.emptyList();
+        }
+        
+        List<Airplane> allUsedAirPlanes = allAirplanes;
+                
+         for (Airplane airplane : allAirplanes) {
+            for (Flight flight : allFlightSince) {
+                if (flight.getAirPlane() != null && !flight.getAirPlane().equals(airplane)) {
+                    allUsedAirPlanes.remove(airplane);
+                }
+            }
+        }
+        return allUsedAirPlanes;
+    }
+
+    @Override
+    public List<Airplane> findByFreeAfterDateTime(LocalDateTime sinceDateTime) {
+        List<Flight> allFlightSince = flightService.getFlightsSince(sinceDateTime);
+        List<Airplane> allAirplanes = airplaneDao.findAll();
+        
+        if(allFlightSince.isEmpty()){
+            return allAirplanes;
+        }
+        
+        List<Airplane> allFreeAirPlanes = allAirplanes;
+                
+         for (Airplane airplane : allAirplanes) {
+            for (Flight flight : allFlightSince) {
+                if (flight.getAirPlane() != null && flight.getAirPlane().equals(airplane)) {
+                    allFreeAirPlanes.remove(airplane);
+                }
+            }
+        }
+        return allFreeAirPlanes;
+    }
+
 }
