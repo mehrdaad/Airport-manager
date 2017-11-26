908f958df00b4ddce98d12e4a00ea820f234be0b
Caky123 <cakl.jan@gmail.com>
2017-11-26 10:17:49 +0100

    AirplaneFacade.java updated

--- Airport-manager-api/src/main/java/cz/fi/muni/pa165/facade/AirplaneFacade.java
+++ Airport-manager-api/src/main/java/cz/fi/muni/pa165/facade/AirplaneFacade.java
@@ -1,6 +1,7 @@
 package cz.fi.muni.pa165.facade;
 
 import cz.fi.muni.pa165.dto.AirplaneDTO;
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -15,12 +16,19 @@ public interface AirplaneFacade {
     
     List<AirplaneDTO> findAll();
     
+    List<AirplaneDTO> findByName(String name);
+    
+    List<AirplaneDTO> findByType(String type);
+    
+    List<AirplaneDTO> findByCapacityMin(int capacity);
+    
+    List<AirplaneDTO> findByCapacityMax(int capacity);
+    
+    List<AirplaneDTO> findByUsedAfterDateTime(LocalDateTime sinceDateTime);
+    
+    List<AirplaneDTO> findByFreeAfterDateTime(LocalDateTime sinceDateTime);
+    
     void deleteAirplane(Long id);
 
     void updateAirplane(AirplaneDTO airplane);
-    
-    /**
-     * TODO non trivial fce
-     */
-    
 }
