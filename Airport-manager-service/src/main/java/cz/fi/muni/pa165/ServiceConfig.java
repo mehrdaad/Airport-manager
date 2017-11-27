bd51c248c31eb4a8a329af08bd78dcafcfee795b
Robert Duriancik <r.duriancik@gmail.com>
2017-11-27 21:44:35 +0100

    Fix LocalDateTime mapping

--- Airport-manager-service/src/main/java/cz/fi/muni/pa165/ServiceConfig.java
+++ Airport-manager-service/src/main/java/cz/fi/muni/pa165/ServiceConfig.java
@@ -7,12 +7,16 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import java.util.Collections;
+
 @Configuration
 @Import(PersistenceConfig.class)
 @ComponentScan("cz.fi.muni.pa165")
 public class ServiceConfig {
     @Bean
     public Mapper dozer() {
-        return new DozerBeanMapper();
+        DozerBeanMapper mapper = new DozerBeanMapper();
+        mapper.setMappingFiles(Collections.singletonList("dozerJdk8Converters.xml")); // enables LocalDateTime mapping
+        return mapper;
     }
 }
