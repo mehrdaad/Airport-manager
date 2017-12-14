ca1d546a77faf2230783524199f1d392ba366941
Robert Duriancik <r.duriancik@gmail.com>
2017-12-14 08:46:02 +0100

    Angular app

---
+++ Airport-manager-web/src/main/webapp/js/angular_app.js
@@ -0,0 +1,31 @@
+'use strict';
+var airportManagerApp = angular.module('airportManagerApp', ['ngRoute', 'managerControllers']);
+var managerControllers = angular.module('managerControllers', []);
+
+/* Configures URL fragment routing  */
+airportManagerApp.config(['$routeProvider',
+    function ($routeProvider) {
+        // $routeProvider.when()
+        //     .otherwise();
+    }
+]);
+
+
+/*
+ * alert closing functions defined in root scope to be available in every template
+ */
+airportManagerApp.run(function ($rootScope) {
+    $rootScope.hideSuccessAlert = function () {
+        $rootScope.successAlert = undefined;
+    };
+    $rootScope.hideWarningAlert = function () {
+        $rootScope.warningAlert = undefined;
+    };
+    $rootScope.hideErrorAlert = function () {
+        $rootScope.errorAlert = undefined;
+    };
+});
+
+/* Controllers */
+
+
