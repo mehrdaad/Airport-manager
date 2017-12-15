e40f430d24a5e0f7657fd9d8b7ff8f49921a22d4
Robert Duriancik <r.duriancik@gmail.com>
2017-12-15 17:05:10 +0100

    Added dependencies needed for RestSpringMvc configuration. Created RestSpringMvcConfig and RestStartInitializer classes.

---
+++ Airport-manager-web/src/main/java/cz/fi/muni/pa165/config/RestSpringMvcConfig.java
@@ -0,0 +1,74 @@
+package cz.fi.muni.pa165.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cz.muni.fi.pa165.sampledata.AirportManagerSampleDataConfiguration;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
+
+/**
+ * Configures a REST application with HATEOAS responses using HAL format.
+ */
+
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
+@EnableWebMvc
+@Configuration
+@Import(AirportManagerSampleDataConfiguration.class)
+@ComponentScan(basePackages = {"cz.fi.muni.pa165.controllers", "cz.fi.muni.pa165.hateoas"})
+public class RestSpringMvcConfig extends WebMvcConfigurerAdapter {
+
+    private static final String HAL_OBJECT_MAPPER_BEAN_NAME = "_halObjectMapper";
+
+    @Autowired
+    private BeanFactory beanFactory;
+
+    @Bean
+    public MappingJackson2HttpMessageConverter customJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(objectMapper());
+        return converter;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = beanFactory.getBean(HAL_OBJECT_MAPPER_BEAN_NAME, ObjectMapper.class);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        // TODO configure for localdatetime if needed
+        return objectMapper;
+    }
+
+    /**
+     * Provides JSR-303 Validator.
+     *
+     * @return JSR-303 validator
+     */
+    @Bean
+    public Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(customJackson2HttpMessageConverter());
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaTypes.HAL_JSON);
+    }
+}
