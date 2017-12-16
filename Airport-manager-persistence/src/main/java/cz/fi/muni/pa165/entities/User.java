0d026bdb4a3da31478947732559fc59ba7d34bde
Robert Duriancik <r.duriancik@gmail.com>
2017-12-16 13:37:27 +0100

    Created User entity. Added not null constraint to mandatory fields in the flight entity.

---
+++ Airport-manager-persistence/src/main/java/cz/fi/muni/pa165/entities/User.java
@@ -0,0 +1,121 @@
+package cz.fi.muni.pa165.entities;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @NotNull
+    @Pattern(regexp = ".+@.+\\....?")
+    private String email;
+
+    @NotNull
+    @Column(nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(nullable = false)
+    private String surname;
+
+    @NotNull
+    @Column(nullable = false)
+    private LocalDateTime registered;
+
+    public User() {
+    }
+
+    private boolean isAdmin;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDateTime getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(LocalDateTime registered) {
+        this.registered = registered;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || !(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        if (isAdmin != user.isAdmin()) return false;
+        if (email != null ? !email.equals(user.getEmail()) : user.getEmail() != null) return false;
+        if (name != null ? !name.equals(user.getName()) : user.getName() != null) return false;
+        if (surname != null ? !surname.equals(user.getSurname()) : user.getSurname() != null) return false;
+        return registered != null ? registered.equals(user.getRegistered()) : user.getRegistered() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = email != null ? email.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + (registered != null ? registered.hashCode() : 0);
+        result = 31 * result + (isAdmin ? 1 : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", registered=" + registered +
+                ", isAdmin=" + isAdmin +
+                '}';
+    }
+}
