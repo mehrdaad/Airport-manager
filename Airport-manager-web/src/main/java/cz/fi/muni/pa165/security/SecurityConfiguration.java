83c85c41a1f33706b222f17eb067ae8d7ed70339
Robert Duriancik <r.duriancik@gmail.com>
2018-01-07 00:01:00 +0100

    Fix url after login.

--- Airport-manager-web/src/main/java/cz/fi/muni/pa165/security/SecurityConfiguration.java
+++ Airport-manager-web/src/main/java/cz/fi/muni/pa165/security/SecurityConfiguration.java
@@ -10,6 +10,13 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -44,7 +51,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login.html")
-                .successForwardUrl("/index.jsp")
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                        httpServletResponse.sendRedirect("/pa165");
+                    }
+                })
                 .permitAll()
                 .and()
                 .logout().logoutUrl("/logout.html").logoutSuccessUrl("/index.jsp").permitAll()
